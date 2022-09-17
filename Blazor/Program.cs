8fd8374c9d180835a4556f8ce505d24eac0565b2
Oliver Schiøtt <oliver.schiott@outlook.com>
2022-09-17 14:23:05 +0200

    Kun kommentar

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddScoped(sp =>
     var t = new HttpClient();
     t.BaseAddress = new Uri("https://innafjord.azurewebsites.net/");
 
-    // TODO: OBS Dette er ikke min group id, må endres.
+    // TODO: OBS Dette bør ikke sjekkes inn i github da det er en pålogging.
     t.DefaultRequestHeaders.Add("GroupId", "Vanndrikkerne");
     t.DefaultRequestHeaders.Add("GroupKey", "F9RQ/S90p0Gvt+DpGnZxxg==");
 
