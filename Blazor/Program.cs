e8c7f4ff49780d3f48bcc36bdf0d8352e6f5c567
Oliver Schiøtt <oliver.schiott@outlook.com>
2022-09-17 13:55:49 +0200

    Add project files.

---
+++ Blazor/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+
+
+
+
+builder.Services.AddScoped(sp =>
+{
+    var t = new HttpClient();
+    t.BaseAddress = new Uri("https://innafjord.azurewebsites.net/");
+
+    // TODO: OBS Dette er ikke min group id, må endres.
+    t.DefaultRequestHeaders.Add("GroupId", "Vanndrikkerne");
+    t.DefaultRequestHeaders.Add("GroupKey", "F9RQ/S90p0Gvt+DpGnZxxg==");
+
+    return t;
+
+    
+});
+
+await builder.Build().RunAsync();
+
