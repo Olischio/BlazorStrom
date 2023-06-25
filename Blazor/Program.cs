737b3184cbf0198f16c208fabd65a5512679b402
Oliver Schiøtt <112466181+Olischio@users.noreply.github.com>
2023-06-25 12:37:32 +0200

    Update Program.cs
    la til tekst

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddScoped(sp =>
     var t = new HttpClient();
     t.BaseAddress = new Uri("https://innafjord.azurewebsites.net/");
 
-    // TODO: OBS Dette bør ikke sjekkes inn i github da det er en pålogging.
+    // TODO: OBS Dette bør ikke sjekkes inn i github da det er en pålogging, men dette er et api for ett fiktift vannverk så det utøver ingen skade.
     t.DefaultRequestHeaders.Add("GroupId", "Vanndrikkerne");
     t.DefaultRequestHeaders.Add("GroupKey", "F9RQ/S90p0Gvt+DpGnZxxg==");
 
